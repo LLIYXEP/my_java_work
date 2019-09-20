1457edc4a97f30c238816cd07bf1e469792ad524
LLIYXEP <scurtu.andrei@yandex.ru>
2019-09-20 16:52:33 +0200

    Work good

--- Test/src/test/Box.java
+++ Test/src/test/Box.java
@@ -1,6 +1,6 @@
 package test;
 
-public class Notebook {
+public class Box {
 	public static void main(String[] args) {
 		System.out.println("S noutbuka i kompa kruto!");
 	}
--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -1,10 +1,35 @@
 package test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test {
 	public static void main(String[] args) {
+		
+		
 		
 	}
 	
 	
+	private Calendar calendar = Calendar.getInstance();
+	public long getDays(Date start, Date end)
+	{
+	  calendar.setTime(start);
+	  calendar.set(Calendar.HOUR_OF_DAY, 0);
+	  calendar.set(Calendar.MINUTE, 0);
+	  calendar.set(Calendar.SECOND, 0);
+	  calendar.set(Calendar.MILLISECOND, 0);
+	    
+	  long l = calendar.getTimeInMillis();
+	    
+	  calendar.setTime(end);
+	  calendar.set(Calendar.HOUR_OF_DAY, 0);
+	  calendar.set(Calendar.MINUTE, 0);
+	  calendar.set(Calendar.SECOND, 0);
+	  calendar.set(Calendar.MILLISECOND, 0);
+	    
+	  l = calendar.getTimeInMillis() - l;
+	  return l / (24 * 60 * 60 * 1000);
+	}
 	
 }
