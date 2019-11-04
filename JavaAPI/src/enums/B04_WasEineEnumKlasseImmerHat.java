6fa18bdaf1c3d176a7f000624670ee29fdad79fe
ascu132 <ascu132@PC14151631.cc-college.gecs.loc>
2019-11-04 12:55:18 +0100

    2

--- JavaAPI/src/enums/B04_WasEineEnumKlasseImmerHat.java
+++ JavaAPI/src/enums/B04_WasEineEnumKlasseImmerHat.java
@@ -1,22 +1,28 @@
 package enums;
 
+import java.util.Arrays;
+
 enum Size {
 	BIG, SMALL
 }
 
 class Groesse {
+	
+	
 	static final Groesse GROSS = new Groesse() {@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "GROSS";
 	}};
 	static final Groesse KLEIN = new Groesse() {
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "KLEIN";
 		}
 	};
+	
+	static Groesse[] values() {
+		return new Groesse[] { GROSS, KLEIN};
+	}
 	
 	private Groesse() {}
 
@@ -49,9 +55,19 @@ public class B04_WasEineEnumKlasseImmerHat {
 		 */
 		s1 = Size.BIG; 
 		g1 = Groesse.GROSS;
-	System.out.println(s1);
-	System.out.println(g1);
-	System.out.println(Groesse.KLEIN);
+		
+		System.out.println(s1);
+		System.out.println(g1);
+		System.out.println(Groesse.KLEIN);
+		
+		/*
+		 * statische Methode values()
+		 */
+		Size[] allSizes = Size.values();
+		System.out.println("Size values: " + Arrays.toString(allSizes));
+		
+		Groesse[] alleGroessen = Groesse.values();
+		System.out.println("Groesse values: " + Arrays.toString(alleGroessen));
 	}
 	
 }
