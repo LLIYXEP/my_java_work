9dc271b4ca971a1447c2b267bc5209eddbc0f9fe
ascu132 <ascu132@PC14151631.cc-college.gecs.loc>
2019-12-09 12:52:40 +0100

    as09

--- Test/src/test/Kreis.java
+++ Test/src/test/Kreis.java
@@ -1,28 +1,15 @@
 package test;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
-import test2.Auto;
-
-
-public class Kreis extends Auto {
+public class Kreis {
 	
-	static int MIN_BJ = 1990;
 	
-	public void start() {
-		System.out.print( test2.Auto.MIN_BJ + " " );
-		System.out.print( Auto.MIN_BJ + " " );
-		System.out.print( MIN_BJ );
-	} 
-
+	
 	public static void main(String[] args) {
 		
-		new Kreis().start();
+		
 		
 	}
-	
-	
-	
-	
 	
 }
--- Test/src/test2/Auto.java
+++ Test/src/test2/Auto.java
@@ -1,6 +1,6 @@
 package test2;
 
-public class Auto {
+public class Auto{
 
 	public static final int MIN_BJ = 1880;
 	
