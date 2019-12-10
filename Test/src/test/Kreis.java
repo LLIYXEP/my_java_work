0fd9f74680f648ce48e6ae3bd9261876467b318c
ascu132 <ascu132@PC14151631.cc-college.gecs.loc>
2019-12-10 12:52:05 +0100

    10s

--- Test/src/test/Kreis.java
+++ Test/src/test/Kreis.java
@@ -1,15 +1,30 @@
 package test;
 
-import java.io.IOException;
+interface Fahrbar {
+	void motorAn();
+	void motorAus();
+	
+	default void fahren() {
+		motorAn();
+		motorAus();
+	}
+}
 
-public class Kreis {
-	
-	
+class Oldtimer implements Fahrbar {
+	public void motorAn() { /* ... */ }
+	public void motorAus( { /* ... */ }
+} 
+
+public class Kreis{
+
 	
 	public static void main(String[] args) {
 		
-		
+
+		TextFormat fmt = new HtmlTextFormat();
+		fmt = new MarkdownTextFormat();
+		System.out.println( fmt.fett("Freitag") );
 		
 	}
-	
+
 }
